a36a0502bc40ea4683fbb2ee6a54171365c20cf0
Justin <justin.gerber48@gmail.com>
2023-05-26 23:51:50 -0600

    Move format_float to format_sfloat is sfloat.py

--- src/sciform/format.py
+++ src/sciform/format.py
@@ -1,20 +1,9 @@
 from typing import Optional, Union
 from dataclasses import dataclass
 import re
-from math import isfinite
-import logging
 
 from sciform.modes import (FillMode, FormatMode, PrecMode, SignMode,
                            GroupingSeparator, DecimalSeparator)
-from sciform.format_utils import (get_mantissa_exp, get_exp_str,
-                                  get_top_and_bottom_digit,
-                                  get_round_digit,
-                                  format_float_by_top_bottom_dig)
-from sciform.grouping import add_separators
-from sciform.prefix import replace_prefix
-
-
-logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -232,74 +221,3 @@ def parse_format_spec(
         default_fmt_spec=default_fmt_spec
     )
 
-
-def format_float(num: float, format_spec: FormatSpec) -> str:
-    format_mode = format_spec.format_mode
-    alternate_mode = format_spec.alternate_mode
-    prec_mode = format_spec.prec_mode
-    prec = format_spec.prec
-    top_padded_digit = format_spec.top_dig_place
-    sign_mode = format_spec.sign_mode
-    capital_exp_char = format_spec.capital_exp_char
-    fill_char = FillMode.to_char(format_spec.fill_mode)
-    if not isfinite(num):
-        if capital_exp_char:
-            return str(num).upper()
-        else:
-            return str(num).lower()
-
-    if format_spec.percent_mode:
-        num *= 100
-
-    exp = format_spec.exp
-    mantissa, exp = get_mantissa_exp(num, format_mode, exp, alternate_mode)
-
-    top_digit, bottom_digit = get_top_and_bottom_digit(mantissa)
-    round_digit = get_round_digit(top_digit, bottom_digit,
-                                  prec, prec_mode)
-
-    mantissa_rounded = round(mantissa, -round_digit)
-
-    if format_mode is not FormatMode.FIXEDPOINT:
-        # Shift the exponent if rounding change the top digit of the mantissa
-        new_top_digit, _ = get_top_and_bottom_digit(mantissa_rounded)
-        exp_shift = new_top_digit - top_digit
-        exp += exp_shift
-        mantissa_rounded = mantissa_rounded * 10**-exp_shift
-
-        # Round again
-        mantissa_rounded = round(mantissa_rounded, -round_digit)
-        if mantissa_rounded == 0:
-            exp = 0
-
-    exp_str = get_exp_str(exp, format_mode, capital_exp_char)
-
-    if mantissa_rounded == -0.0:
-        mantissa_rounded = abs(mantissa_rounded)
-    mantissa_str = format_float_by_top_bottom_dig(mantissa_rounded,
-                                                  top_padded_digit,
-                                                  round_digit, sign_mode,
-                                                  fill_char)
-
-    # TODO: Think about the interaction between separators and fill
-    #  characters
-    thousands_separator = GroupingSeparator.to_char(
-        format_spec.thousands_separator)
-    decimal_separator = DecimalSeparator.to_char(format_spec.decimal_separator)
-    thousandths_separator = GroupingSeparator.to_char(
-        format_spec.thousandths_separator)
-    mantissa_str = add_separators(mantissa_str,
-                                  thousands_separator,
-                                  decimal_separator,
-                                  thousandths_separator,
-                                  group_size=3)
-
-    full_str = f'{mantissa_str}{exp_str}'
-
-    if format_spec.prefix_mode:
-        full_str = replace_prefix(full_str)
-
-    if format_spec.percent_mode:
-        full_str = full_str + '%'
-
-    return full_str
