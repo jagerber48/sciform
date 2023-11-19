02e996322eea4df9c97249b043478b909d2e9b70
Justin <justin.gerber48@gmail.com>
2023-11-19 08:19:30 -0700

    Move option kwarg documentation into Formatter (and update text)

--- src/sciform/formatter.py
+++ src/sciform/formatter.py
@@ -13,17 +13,17 @@ from sciform.format_utils import Number
 class Formatter:
     """
     :class:`Formatter` is used to convert numbers and pairs of numbers
-    into formatted strings. Formatting options are configured using
-    :class:`FormatOptions`. Any unpopulated format options will be
-    populated from the global default options at format time. See
-    :ref:`formatting_options` for more details on the available options.
-    If no options are provided then the global default options are used
-    for all options.
+    into formatted strings according to a variety of formatting options.
+    See :ref:`formatting_options` for more details on the available
+    options. Any options which are unpopulated (have the value ``None``)
+    will be populated at format time by the corresponding values in the
+    globally configured default options. See :ref:`global_config` for
+    details about how to view and modify the global default options.
 
     >>> from sciform import Formatter
     >>> sform = Formatter(exp_mode='engineering',
     ...                   round_mode='sig_fig',
-    ...                   ndigits=4))
+    ...                   ndigits=4)
     >>> print(sform(12345.678))
     12.35e+03
 
@@ -32,25 +32,12 @@ class Formatter:
 
     >>> sform = Formatter(exp_mode='engineering',
     ...                   round_mode='sig_fig',
-    ...             ndigits=2))
+    ...             ndigits=2)
     >>> print(sform(12345.678, 3.4))
     (12.3457 ± 0.0034)e+03
 
-    :class:`FormatOptions` instances store all the configuration options
-    used to format numbers and number/uncertainty pairs. See
-    :ref:`formatting_options` for more details on the available options.
-    :class:`FormatOptions` instances are used to create
-    :class:`Formatter` instances and to modify the global default
-    configuration.
-
-    It is not necessary to provide input for all options. At format
-    time, any un-populated options (indicated by a ``None`` value) will
-    be populated with the corresponding options from the global default
-    options. See :ref:`global_config` for details about how to view and
-    modify the global default options.
-
     The following checks are performed when creating a new
-    :class:`FormatOptions` object:
+    :class:`Formatter` object:
 
     * ``ndigits`` >= 1 for significant figure rounding mode
     * ``exp_val`` must be consistent with the exponent mode. If
@@ -62,100 +49,110 @@ class Formatter:
         engineering modes
       * ``exp_val`` must be a multiple of 10 for binary iec mode
 
-    * ``upper_separator`` may be any :class:`GroupingSeparator` but must
-      be different from ``decimal_separator``
-    * ``decimal_separator`` may only be :class:`GroupingSeparator.POINT`
-      or :class:`GroupingSeparator.COMMA`
-    * ``lower_separator`` may only be :class:`GroupingSeparator.NONE`,
-      :class:`GroupingSeparator.SPACE`, or
-      :class:`GroupingSeparator.UNDERSCORE`
+    * ``upper_separator`` may be any of ``['', ',', '.', ' ', '_']`` but
+      must be different from ``decimal_separator``
+    * ``decimal_separator`` may be any of ``['.', ',']``
+    * ``lower_separator`` may be any of ``['', ' ', '_']``
     * if ``pdg_sig_figs=True`` then ``ndigits=None`` or
       ``ndigits=AutoDigits``.
 
-    :param exp_mode: :class:`ExpMode` indicating the formatting
-      mode to be used.
-    :param exp_val: :class:`int` or :class:`AutoExpVal` sentinel
-      indicating a value which must be used for the exponent. If
-      specified, this value must be 0 for fixed point and percent modes,
-      an integer multiple of 3 for engineering and engineering shifted
-      modes, and an integer multiple of 10 for binary IEC mode.
-    :param round_mode: :class:`RoundMode` indicating whether to round
-      the number based on significant figures or decimal places.
-    :param ndigits: :class:`int` or :class:`AutoDigits` sentinel
-      indicating how many significant digits or which decimal place to
-      use for rounding. Must be >= 1 for significant figure rounding.
-      May be any integer for decimal place rounding.
-    :param upper_separator: :class:`GroupingSeparator` indicating the
-      character to be used to group digits above the decimal symbol.
-    :param decimal_separator: :class:`GroupingSeparator` indicating
-      the character to be used as the decimal symbol.
-      :class:`GroupingSeparator.POINT` or
-      :class:`GroupingSeparator.COMMA`. Note that ``decimal_separator``
-      cannot be the same as ``upper_separator``
-    :param lower_separator: :class:`GroupingSeparator` indicating the
-      character to be used to group digits below the decimal symbol.
-      :class:`GroupingSeparator.NONE`, :class:`GroupingSeparator.SPACE`,
-      or :class:`GroupingSeparator.UNDERSCORE`.
-    :param sign_mode: :class:`SignMode` indicating sign
-      symbol behavior.
-    :param fill_mode: :class:`FillMode` indicating whether
-      to fill with zeros or spaces.
+    :param exp_mode: Specify the formatting mode.
+    :type exp_mode: ``Literal['fixed_point', 'percent', 'scientific',
+      'engineering', 'engineering_shifted', 'binary', 'binary_iec']``
+    :param exp_val: Indicates how the exponent value should be chosen.
+      If an integer is specified, the value must be 0 for fixed point
+      and percent modes, an integer multiple of 3 for engineering and
+      engineering shifted modes, and an integer multiple of 10 for
+      binary IEC mode.
+    :type exp_val: ``Union[int, type[AutoExpVal]]``
+    :param round_mode: Indicate how to round numbers during formatting.
+    :type round_mode: ``Literal['sig_fig', 'dec_place']``
+    :param ndigits: Indicate how the many significant digits or the
+      decimal place to use for rounding. Must be >= 1 for significant
+      figure rounding. Can be any integer for decimal place rounding.
+    :type ndigits: ``Union[int, type[AutoDigits]]``
+    :param upper_separator: Separator character to be used to group
+      digits above the decimal symbol.
+    :type upper_separator: ``Literal['', ',', '.', ' ', '_']``
+    :param decimal_separator: Separator character to be used as the
+      decimal symbol. Note that ``decimal_separator`` cannot be the same
+      as ``upper_separator``
+    :type decimal_separator: ``Literal['.', ',']``
+    :param lower_separator: Separator character to be used to group
+      digits below the decimal symbol.
+    :type lower_separator: ``Literal['', ' ', '_']``
+    :param sign_mode: :class:`SignMode` indicating sign symbol behavior.
+    :type sign_mode: ``Literal['-', '+', ' ']``
+    :param fill_mode: :class:`FillMode` indicating whether to fill with
+      zeros or spaces.
+    :type fill_mode: ``Literal[' ', '0']``
     :param top_dig_place: Positive ``int`` indicating the digits place
       to which the string will be left padded before the sign symbol. 0
       corresponds to the ones place, 1 corresponds to the tens place
       etc. E.g. ``top_dig_place=4`` will convert ``12`` into ``00012``.
-    :param exp_format: :class:`ExpFormat` indicating if exponents should
-      be displayed in standard ``e+01`` format, if they should be
-      translated into SI or IEC prefixes, or if they should be
-      translated into "parts-per" format.
-    :param extra_si_prefixes: ``dict[int, Union[str, None]]`` mapping
-      additional exponent values to si prefixes. Entries overwrite
-      default values. A value of ``None`` means that exponent will not
-      be converted.
-    :param extra_iec_prefixes: ``dict[int, Union[str, None]]`` mapping
-      additional exponent values to iec prefixes. Entries overwrite
-      default values. A value of ``None`` means that exponent will not
-      be converted.
-    :param extra_parts_per_forms: ``dict[int, Union[str, None]]``
-      mapping additional exponent values to "parts-per" forms. Entries
-      overwrite default values. A value of ``None`` means that exponent
-      will not be converted.
-    :param capitalize: :class:`bool` indicating whether the
-      exponentiation symbol should be upper- or lower-case.
-    :param superscript_exp: :class:`bool` indicating if the exponent
-      string should be converted into superscript notation. E.g.
-      ``'1.23e+02'`` is converted to ``'1.23×10²'``
-    :param latex: :class:`bool` indicating if the resulting string
-      should be converted into a latex parseable code, e.g.
+    :type top_dig_place: ``int``
+    :param exp_format: :class:`ExpFormat` indicating how exponents
+      should be presented.
+    :type exp_format: ``Literal['standard', 'prefix', 'parts_per']``
+    :param extra_si_prefixes: Dictionary mapping additional exponent
+      values to si prefixes. Entries overwrite default values. A value
+      of ``None`` means that exponent will not be converted.
+    :type extra_si_prefixes: ``dict[int, Union[str, None]]``
+    :param extra_iec_prefixes: Dicitonary mapping additional exponent
+      values to iec prefixes. Entries overwrite default values. A value
+      of ``None`` means that exponent will not be converted.
+    :type extra_iec_prefixes: ``dict[int, Union[str, None]]``
+    :param extra_parts_per_forms: Dictionary mapping additional exponent
+      values to "parts-per" forms. Entries overwrite default values. A
+      value of ``None`` means that exponent will not be converted.
+    :type extra_parts_per_forms: ``dict[int, Union[str, None]]``
+    :param capitalize: Flag indicating whether the exponentiation symbol
+      should be upper- or lower-case.
+    :type capitalize: ``bool``
+    :param superscript_exp: Flag indicating if the exponent string
+      should be converted into superscript notation. E.g. ``'1.23e+02'``
+      is converted to ``'1.23×10²'``
+    :type superscript_exp: ``bool``
+    :param latex: Flag indicating if the resulting string should be
+      converted into a latex parseable code, e.g.
       ``'\\left(1.23 \\pm 0.01\\right)\\times 10^{2}'``.
-    :param nan_inf_exp: :class:`bool` indicating whether non-finite
-      numbers such as ``float('nan')`` or ``float('inf')`` should be
-      formatted with exponent symbols when exponent modes including
-      exponent symbols are selected.
-    :param bracket_unc: :class:`bool` indicating if bracket uncertainty
-      mode (e.g. ``12.34(82)`` instead of ``12.34 +/- 0.82``) should be
+    :type latex: ``bool``
+    :param nan_inf_exp: Flag indicating whether non-finite numbers such
+      as ``float('nan')`` or ``float('inf')`` should be formatted with
+      exponent symbols when exponent modes including exponent symbols
+      are selected.
+    :type nan_inf_exp: ``bool``
+    :param bracket_unc: Flag indicating if bracket uncertainty mode
+      (e.g. ``12.34(82)`` instead of ``12.34 ± 0.82``) should be
       used.
-    :param pdg_sig_figs: :class:`bool` indicating whether the
-      particle-data-group conventions should be used to automatically
-      determine the number of significant figures to use for
-      uncertainty.
-    :param val_unc_match_widths: :class:`bool` indicating if the value
-      or uncertainty should be left padded to ensure they are both left
+    :type bracket_unc: ``bool``
+    :param pdg_sig_figs: Flag indicating whether the particle-data-group
+      conventions should be used to automatically determine the number
+      of significant figures to use for uncertainty.
+    :type pdg_sig_figs: ``bool``
+    :param val_unc_match_widths: Flag indicating if the value or
+      uncertainty should be left padded to ensure they are both left
       padded to the same digits place.
-    :param bracket_unc_remove_seps: :class:`bool` indicating if
-      separator symbols should be removed from the uncertainty when
-      using bracket uncertainty mode. E.g. expressing ``123.4 +/- 2.3``
-      as ``123.4(23)`` instead of ``123.4(2.3)``.
-    :param unc_pm_whitespace: :class:`bool` indicating if there should be
-      whitespace surrounding the ``'+/-'`` symbols when formatting. E.g.
-      ``123.4+/-2.3`` compared to ``123.4 +/- 2.3``.
-    :param add_c_prefix: :class:`bool` (default ``False``) if ``True`` adds
+    :type val_unc_match_widths: ``bool``
+    :param bracket_unc_remove_seps: Flag indicating if separator symbols
+      should be removed from the uncertainty when using bracket
+      uncertainty mode. E.g. expressing ``123.4 ± 2.3`` as
+      ``123.4(23)`` instead of ``123.4(2.3)``.
+    :type bracket_unc_remove_seps: ``bool``
+    :param unc_pm_whitespace: Flag indicating if there should be
+      whitespace surrounding the ``'±'`` symbols when formatting. E.g.
+      ``123.4±2.3`` compared to ``123.4 ± 2.3``.
+    :type unc_pm_whitespace: ``bool``
+    :param add_c_prefix: (default ``False``) If ``True``, adds
       ``{-2: 'c'}`` to ``extra_si_prefixes``.
-    :param add_small_si_prefixes: ``bool`` (default ``False``) if
-      ``True`` adds ``{-2: 'c', -1: 'd', +1: 'da', +2: 'h'}`` to
+    :type add_c_prefix: ``bool``
+    :param add_small_si_prefixes: (default ``False``) If ``True``, adds
+      ``{-2: 'c', -1: 'd', +1: 'da', +2: 'h'}`` to
       ``extra_si_prefixes``.
-    :param add_ppth_form: :class:`bool` (default ``False``) if ``True`` adds
+    :type add_small_si_prefixes: ``bool``
+    :param add_ppth_form: (default ``False``) if ``True``, adds
       ``{-3: 'ppth'}`` to ``extra_parts_per_forms``.
+    :type add_ppth_form: ``bool``
     """
     def __init__(
             self,
